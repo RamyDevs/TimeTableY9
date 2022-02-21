9b763d26eeed89323e629531851d0179ac83a386
RamyDevs <66511353+RamyDevs@users.noreply.github.com>
2022-02-21 22:08:53 +0200

    Add files via upload

---
+++ assets/scripts/database.js
@@ -0,0 +1,33 @@
+blockA = null;
+blockB = null;
+blockC = null;
+blockD = null;
+blockE = null;
+blockF = null;
+p1 = null;
+p2 = null
+
+blue = ["Math","English"]
+yellow = ["English","Math"]
+
+sundaytA = [blockD,blockF,p1,blockE,blockB]
+mondaytA = ["PE", blockB,p2,"Arabic",blockC]
+tuesdaytA = [blockE,blockA,p1,blockB,blockC]
+wednesdaytA = [p1,"Arabic", blockE, blockD,p2]
+thursdaytA = [blockA,"PE",p2,blockC,blockF]
+sundaytB = [blockE,blockB,p1,blockD,p2]
+mondaytB = [blockA,blockF, blockC, "Arabic", blockD]
+tuesdaytB = [blockB,blockA,blockF,"PE", blockE]
+wednesdaytB = ["Arabic", blockC, p2, "HRCF", p1]
+thursdaytB = [p1, p2, blockF, blockD, blockA]
+
+YahyaK = ["Biology","Physics","Business","Computer Science","Chemistry","French" , blue[0],blue[1]]
+AdamE = ["Physics","History","Chemistry",'Drama','English Literature',"French",yellow[0],yellow[1]]
+RamyW = ["Physics","Business","Chemistry","Computer Science","Economics","Biology",yellow[0],yellow[1]]
+LouayE = ["Physics","History","Economics","Drama","English Literature","Biology",yellow[0],yellow[1]]
+AbdallahS = ["Chemistry","History","Business","Computer Science","ICT","Biology",blue[0],blue[1]]
+SeifA = ["Physics","Computer Science","Chemistry","Business","ICT","French",blue[0],blue[1]]
+SherifN = ["Physics","Computer Science","Chemistry","Business","ICT","Biology",blue[0],blue[1]]
+YehiaE = ["Geography","Physics","Business","Computer Science","Chemistry","Biology",blue[0],blue[1]]
+AlberG = ["Biology","Computer Science","Chemistry","Computer Science","English Literature","Physics",yellow[0],yellow[1]]
+AliF = ["Geography","History","Economics","Drama","English Literature","Physics",yellow[0],yellow[1]]
